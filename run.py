f904dc28fc416207af793923f493299164c45e38
Caleb GT <calebju4@gatech.edu>
2024-02-20 16:24:38 -0500

    working genetic

--- run.py
+++ run.py
@@ -45,6 +45,45 @@ class SimpleLogger():
             np.savetxt(fp, self.data_arr[:self.ct], fmt=fmt)
         print(f"Saved testing logs to {self.fname}")
 
+def validate(params, get_action):
+    """ Test/validates policy.
+    :param get_action: general function that takes in a current observation and outputs an action
+    """
+    nhistory = 10
+    start_index = 76*4*24
+    end_index = -1
+    data = "real"
+
+    logger = SimpleLogger(params["fname"], params["env_mode"])
+
+    # new environment for testing 
+    n_steps = (90*4*24)-start_index
+    env = gym.make(
+        "gym_examples/BatteryEnv-v0", 
+        nhistory=nhistory, 
+        data=data, 
+        mode=params["env_mode"], 
+        avoid_penalty=True,
+        start_index=start_index,
+        max_episode_steps=n_steps, # can change length here!"
+    )
+    try:
+        obs, info = env.reset()
+        total_reward = 0
+        for _ in range(n_steps):
+            action = get_action(obs)
+            obs, reward, terminated, truncated, info = env.step(int(action))
+            total_reward += reward
+            logger.store((obs, action, total_reward))
+            if terminated or truncated:
+                obs, info = env.reset()
+
+        logger.save()
+        print(f"Total reward: {total_reward}")
+
+    except KeyboardInterrupt:
+        logger.save()
+
 def run_qlearn(params):
     """ Runs DQN experiment 
 
@@ -53,12 +92,6 @@ def run_qlearn(params):
     assert "env_mode" in params
     assert "seed" in params
     assert "train_len" in params
-
-    fname = os.path.join(
-        "logs", 
-        f"alg=dqn_data=real_env_mode={params['env_mode']}_train_len={params['train_len']}_norm_obs={params['norm_obs']}_seed={params['seed']}.csv"
-    )
-    logger = SimpleLogger(fname, params["env_mode"])
 
     nhistory = 10
     env = gym.make(
@@ -89,38 +122,86 @@ def run_qlearn(params):
     )
     model.learn(total_timesteps=params["train_len"], log_interval=12)
 
-    # new environment for testing 
+    lows, highs = env.observation_space.low, env.observation_space.high
+    rng = highs - lows
+    def get_action(obs):
+        rescaled_obs = np.divide(obs - lows, rng)
+        action, _ = model.predict(obs, deterministic=True)
+        return action
+
+    fname = os.path.join(
+        "logs", 
+        f"alg=dqn_data=real_env_mode={params['env_mode']}_train_len={params['train_len']}_norm_obs={params['norm_obs']}_seed={params['seed']}.csv"
+    )
+    params["fname"] = fname
+    validate(params, get_action)
+
+def run_bangbang_offline(params):
+    (buy_price, sell_price) = (54.11, 114) # computed by Genetic algorithm offline
+    sell, null, buy = 0, 1, 2
+
+    def get_action(obs):
+        last_lmp = obs[1]
+        if last_lmp >= sell_price:
+            a = sell
+        elif last_lmp <= buy_price:
+            a = buy
+        else:
+            a = null
+        return a
+
+    fname = os.path.join("logs", f"alg=bang_offline_data=real_env_mode={params['env_mode']}.csv")
+    params["fname"] = fname
+    validate(params, get_action)
+
+def run_bangbang_online(params):
+    """ Use genetic algorithm to evaluate best cut-offs """
+    nhistory = 10
+    data = "real"
+    start_index = 76*4*24,
+    end_index = -1
+    env_mode = "delay"
+
+    # collect all the prices
     env = gym.make(
         "gym_examples/BatteryEnv-v0", 
         nhistory=nhistory, 
-        # data="periodic", 
-        mode=params["env_mode"], 
-        avoid_penalty=True,
+        data=data, 
+        mode=env_mode, 
+        max_episode_steps=14*4*24, # can change length here!"
     )
-    env._max_episode_steps = 672
-    lows, highs = env.observation_space.low, env.observation_space.high
-    rng = highs - lows
-    if params.get("norm_obs", False):
-        env = gym.wrappers.TransformObservation(env, lambda obs : np.divide(obs - lows, rng))
+    env.reset()
 
-    try:
-        obs, info = env.reset()
-        total_reward = 0
-        for _ in range(1024):
-            action, _states = model.predict(obs, deterministic=True)
-            obs, reward, terminated, truncated, info = env.step(int(action))
-            total_reward += reward
-            logger.store((obs, action, total_reward))
-            if terminated or truncated:
-                obs, info = env.reset()
+    def get_action(obs):
+        
+        def objective(bounds):
+            """ Evaluate profit with our strategy
+            :param x: lower and upper bound for buy and sell price, respetictively
+            """
+            buy_price, sell_price = bounds
+            if buy_price > sell_price:
+                return 0
 
-        logger.save()
-        print(f"Total reward: {total_reward}")
+            sell, null, buy = 0, 1, 2
+            s, _ = env.reset(options={"start": start_idx})
+            last_lmp = s[1]
+            total_rwd = 0
+            while 1:
+                if last_lmp >= sell_price:
+                    a = sell
+                elif last_lmp <= buy_price:
+                    a = buy
+                else:
+                    a = null
+                s, r, term, trunc, _ = env.step(a)
+                last_lmp = s[1]
+                total_rwd += r
+                if term or trunc:
+                    break
+            return -total_rwd
 
-    except KeyboardInterrupt:
-        logger.save()
-
-def run_bangbang(params):
+    lbs, ubs = [-25, 0], [100, 200]
+    x = genetic.optimize(objective, lbs, ubs, n_iter=100, n_pop=50, seed=None)
 
 def run_parallel_exp(params, num_exps):
     """
@@ -165,4 +246,5 @@ if __name__ == "__main__":
     if params["parallel"]:
         run_parallel_exp(params, params["seed"])
     else:
-        run_exp(params)
+        # run_qlearn(params)
+        run_bangbang_offline(params)
