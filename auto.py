7c3d9af6452ee8ff106755eca90b3fcdb29c438e
Caleb Ju <cju33@login31.chn.perlmutter.nersc.gov>
2024-03-31 18:02:12 -0700

    iris's auto file preferred

--- auto.py
+++ auto.py
@@ -5,6 +5,8 @@ import os
     # os.system(cmd)
     # cmd = f"python run.py --env_mode delay --seed {i} --train_len 100000 --norm_obs --more_data"
     # os.system(cmd)
+   #  cmd = f"python run.py --train_len 200000 --more_data --norm_obs --norm_rwd --daily_cost 100 --learning_rate 0.000918 --exploration_fraction 0.9685 --seed {i}"
+    # os.system(cmd)
 
-cmd = f"python run.py --train_len 500000 --more_data --norm_obs --norm_rwd --daily_cost 100 --learning_rate 0.000918 --exploration_fraction 0.9685"
+cmd = f"python run.py --train_len 200000 --more_data --norm_obs --norm_rwd --daily_cost 100 --learning_rate 0.000918 --exploration_fraction 0.9685 --n_trials 10 --seed 0"
 os.system(cmd)
