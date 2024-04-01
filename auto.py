0b4020f56006ad8114c599fa8db837ff1309e037
Caleb Ju <cju33@login31.chn.perlmutter.nersc.gov>
2024-03-31 18:43:05 -0700

    add solar coloc

--- auto.py
+++ auto.py
@@ -10,3 +10,6 @@ import os
 
 cmd = f"python run.py --train_len 200000 --more_data --norm_obs --norm_rwd --daily_cost 100 --learning_rate 0.000918 --exploration_fraction 0.9685 --n_trials 10 --seed 0"
 os.system(cmd)
+
+cmd = f"python run.py --train_len 200000 --more_data --norm_obs --norm_rwd --daily_cost 100 --learning_rate 0.000918 --exploration_fraction 0.9685 --n_trials 10 --seed 0 --solar_coloc"
+os.system(cmd)
